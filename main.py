ee8f9eeb5c77a9c2c5aadd3f44738f8fc7595a20
JackTheYouTuber <naveedreza605@gmail.com>
2022-08-03 03:55:00 +0600

    Command does not work yet

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email import message
 import hikari
 from Cogs import __Bot_Functions__
 
@@ -5,7 +6,7 @@ TOKEN = "Token"
 bot = hikari.GatewayBot(token=TOKEN)
 
 @bot.listen(hikari.GuildMessageCreateEvent)
-async def Message_Events(event:hikari.events.GuildMessageCreateEvent):
+async def Log_Events(event:hikari.events.GuildMessageCreateEvent):
     message = event.content
     message_author = event.author
     message_channel_location = event.get_channel()
@@ -14,8 +15,17 @@ async def Message_Events(event:hikari.events.GuildMessageCreateEvent):
     Log_Text = f"[{message_author}] :> {message} | [Channel({message_channel_location})]"
     __Bot_Functions__.Log(Log_Text=Log_Text)
 
+@bot.listen(hikari.GuildChannelCreateEvent)
+async def Bot_Command(event:hikari.events.GuildMessageCreateEvent):
+    message = event.content
+    message_author = event.author
+    message_channel_location = event.get_channel()
+    message_channel_location_id = event.channel_id
+
     # Bot Commands
     Bot_Command = __Bot_Functions__.Command(message, message_author, message_channel_location)
-    bot.rest.create_message(content=Bot_Command, channel=event.channel_id)    
+    await bot.rest.create_message(content=Bot_Command, channel=message_channel_location_id)    
+
+
 
 bot.run()
